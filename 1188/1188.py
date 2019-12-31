2d41b03b87f4621852a5da86fd19a80da6a8828d
Erick R. Ribeiro <erick.ribeiro.16@gmail.com>
2019-12-31 08:07:58 -0400

    problem 1188 was resolved

---
+++ 1188/1188.py
@@ -0,0 +1,25 @@
+op = input()
+# M =  [[i+j for j in range(12)] for i in range(12)]
+
+total = 0
+count = 0
+n = 12
+for i in range(n):
+    for j in range(n):
+        value = float(input())
+        if i + j > n -1:
+            if i > j:
+                # M[i][j] = value
+                # print('\t', M[i][j], end=" ")
+                # count += M[i][j]
+                count += value
+                # total += 1
+                continue
+    #     print('\t', 0, end=" ")
+    # print()
+# print(total)
+
+if op == "S":   
+    print("{0:.1f}".format(count))
+else:
+    print("{0:.1f}".format(count/30))
